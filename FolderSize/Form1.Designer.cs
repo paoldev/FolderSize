149d78fc55f884b83751931fadd98d45ec564a6e
paoldev <55021433+paoldev@users.noreply.github.com>
2024-09-21 11:53:56 +0200

    Update Form title.

--- FolderSize/Form1.Designer.cs
+++ FolderSize/Form1.Designer.cs
@@ -31,103 +31,102 @@
             System.Windows.Forms.Label label2;
             System.Windows.Forms.Label label3;
             System.Windows.Forms.Label label4;
-            this.selectDrive = new System.Windows.Forms.ComboBox();
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label5 = new System.Windows.Forms.Label();
+            selectDrive = new System.Windows.Forms.ComboBox();
+            treeView1 = new System.Windows.Forms.TreeView();
+            label1 = new System.Windows.Forms.Label();
+            progressBar1 = new System.Windows.Forms.ProgressBar();
+            label5 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
             label3 = new System.Windows.Forms.Label();
             label4 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // selectDrive
-            // 
-            this.selectDrive.FormattingEnabled = true;
-            this.selectDrive.Location = new System.Drawing.Point(86, 12);
-            this.selectDrive.Name = "selectDrive";
-            this.selectDrive.Size = new System.Drawing.Size(121, 24);
-            this.selectDrive.TabIndex = 0;
-            this.selectDrive.SelectedIndexChanged += new System.EventHandler(this.selectDrive_SelectedIndexChanged);
-            // 
-            // treeView1
-            // 
-            this.treeView1.Location = new System.Drawing.Point(1, 120);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(597, 383);
-            this.treeView1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(328, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "0";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(1, 82);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(597, 32);
-            this.progressBar1.TabIndex = 3;
+            SuspendLayout();
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(-2, 15);
+            label2.Location = new System.Drawing.Point(-2, 14);
             label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(82, 17);
+            label2.Size = new System.Drawing.Size(67, 15);
             label2.TabIndex = 4;
             label2.Text = "Select drive";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new System.Drawing.Point(236, 15);
+            label3.Location = new System.Drawing.Point(206, 14);
             label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(71, 17);
+            label3.Size = new System.Drawing.Size(55, 15);
             label3.TabIndex = 5;
             label3.Text = "Total Size";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new System.Drawing.Point(236, 48);
+            label4.Location = new System.Drawing.Point(206, 45);
             label4.Name = "label4";
-            label4.Size = new System.Drawing.Size(76, 17);
+            label4.Size = new System.Drawing.Size(63, 15);
             label4.TabIndex = 6;
             label4.Text = "Directories";
             // 
+            // selectDrive
+            // 
+            selectDrive.FormattingEnabled = true;
+            selectDrive.Location = new System.Drawing.Point(75, 11);
+            selectDrive.Name = "selectDrive";
+            selectDrive.Size = new System.Drawing.Size(106, 23);
+            selectDrive.TabIndex = 0;
+            selectDrive.SelectedIndexChanged += selectDrive_SelectedIndexChanged;
+            // 
+            // treeView1
+            // 
+            treeView1.Location = new System.Drawing.Point(1, 112);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new System.Drawing.Size(523, 359);
+            treeView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(287, 14);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(13, 15);
+            label1.TabIndex = 2;
+            label1.Text = "0";
+            label1.Click += label1_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new System.Drawing.Point(1, 77);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new System.Drawing.Size(522, 30);
+            progressBar1.TabIndex = 3;
+            // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(328, 48);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(16, 17);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "0";
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(287, 45);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(13, 15);
+            label5.TabIndex = 7;
+            label5.Text = "0";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(599, 504);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(label4);
-            this.Controls.Add(label3);
-            this.Controls.Add(label2);
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.selectDrive);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(524, 472);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(progressBar1);
+            Controls.Add(label1);
+            Controls.Add(treeView1);
+            Controls.Add(selectDrive);
+            Name = "Form1";
+            Text = "FolderSize";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
